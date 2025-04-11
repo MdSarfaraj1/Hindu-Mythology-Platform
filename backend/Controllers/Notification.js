cb79d644acc3a116f7477a319792b2fd03ab2541
MdSarfaraj1 <mahammadsarfaraj6@gmail.com>
2025-04-11 16:12:17 +0530

    hi

--- backend/Controllers/Notification.js
+++ backend/Controllers/Notification.js
@@ -3,8 +3,19 @@ const User = require("../Models/User");
 const Story =require("../Models/Stories") 
 const admin = require("firebase-admin");
 const { generateStory } = require("../Utils/generateStory");
-//const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
-const serviceAccount=require("../firbase.json")
+const serviceAccount = {
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL, 
+  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+};
 
 // Initialize Firebase Admin once
 admin.initializeApp({
